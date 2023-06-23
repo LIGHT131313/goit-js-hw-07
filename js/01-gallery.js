ec044b873616bbe74033234da252bb2cd1ecea9a
LIGHT131313 <131313light@gmail.com>
2023-06-23 18:44:24 +0300

    pre-release version

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+function createMarkupItems(arr) {
+	return arr
+		.map(
+			({ preview, original, description }) =>
+				`<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"/>
+                    </a>
+                </li>`
+		)
+		.join('');
+}
+list.insertAdjacentHTML('beforeend', createMarkupItems(galleryItems));
+
+list.addEventListener('click', onClickZoomImg);
+
+function onClickZoomImg(evt) {
+	evt.preventDefault();
+
+	if (!evt.target.classList.contains('gallery__image')) {
+		return;
+	}
+
+	const createMarkupModal = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600" alt="${evt.target.alt}">
+`);
+	createMarkupModal.show();
+}
